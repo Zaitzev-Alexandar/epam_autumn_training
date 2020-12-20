6d6e621394e67db19d23a97441a245254f24ecb7
Alexandar Zaitzev <zaitzev1@gmail.com>
2020-12-20 15:17:46 +0300

    task03 - colors

---
+++ task03/ColorsLib/Color.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorsLib
+{
+    /// <summary>
+    /// Colors enumeration.
+    /// </summary>
+    public enum Color
+    {
+        /// <summary>
+        /// Red.
+        /// </summary>
+        Red,
+        /// <summary>
+        /// Orange.
+        /// </summary>
+        Orange,
+        /// <summary>
+        /// Yellow.
+        /// </summary>
+        Yellow,
+        /// <summary>
+        /// Green.
+        /// </summary>
+        Green,
+        /// <summary>
+        /// Blue.
+        /// </summary>
+        Blue,
+        /// <summary>
+        /// Purple.
+        /// </summary>
+        Purple,
+        /// <summary>
+        /// Black.
+        /// </summary>
+        Black,
+        /// <summary>
+        /// Grey
+        /// </summary>
+        Grey,
+        /// <summary>
+        /// White.
+        /// </summary>
+        White,
+        /// <summary>
+        /// Brown
+        /// </summary>
+        Brown,
+        /// <summary>
+        /// Colorless
+        /// </summary>
+        Colorless
+    }
+}
