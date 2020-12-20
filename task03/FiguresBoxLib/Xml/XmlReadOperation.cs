57b284f3a39e39c2a12f4887f6f8e968220bdb1b
Alexandar Zaitzev <zaitzev1@gmail.com>
2020-12-20 16:46:52 +0300

    task03 - read/write operations

---
+++ task03/FiguresBoxLib/Xml/XmlReadOperation.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using FiguresLib.PaperFigures;
+using FiguresLib.FilmFigures;
+using FiguresLib.PlasticFigures;
+using FiguresLib.Interfaces;
+
+namespace FiguresBoxLib.Xml
+{
+    /// <summary>
+    /// Method enumerations for reading xml files.
+    /// </summary>
+    public enum XmlReadType
+    {
+        /// <summary>
+        /// StreamReader.
+        /// </summary>
+        StreamReader,
+        /// <summary>
+        /// XmlReader.
+        /// </summary>
+        XmlReader
+    }
+    /// <summary>
+    /// Class describing reading xml file.
+    /// </summary>
+    internal class XmlReadOperation
+    {
+        /// <summary>
+        /// Reading figures from xml file using StreamReader.
+        /// </summary>
+        /// <param name="filePath">File path.</param>
+        /// <returns></returns>
+        public IFigure[] ReadXmlWithStreamReader(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string xmlString = reader.ReadToEnd();
+
+                XmlDocument document = new XmlDocument();
+                document.LoadXml(xmlString);
+
+                XmlNodeList list = document.GetElementsByTagName("figure");
+
+                IFigure[] figures = InitFiguresArray();
+
+                // Fill array with figures.
+                int index = 0;
+                foreach (XmlNode node in list)
+                {
+                    if (index != figures.Length)
+                    {
+                        figures[index] = GetFigure(node);
+                        index++;
+                    }
+                    else
+                    {
+                        return figures;
+                    }
+                }
+
+                return figures;
+            }
+        }
+
+        /// <summary>
+        /// Reading figures from xml file using XmlReader.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу.</param>
+        /// <returns></returns>
+        public IFigure[] ReadXmlWithXmlReader(string filePath)
+        {
+            using (XmlReader reader = XmlReader.Create(filePath))
+            {
+                IFigure[] figures = InitFiguresArray();
+                int index = 0;
+
+                string element = "";
+
+                string material = "";
+                string form = "";
+
+                int length = 0;
+                int width = 0;
+                int radius = 0;
+
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        element = reader.Name;
+                        if (element == "figure")
+                        {
+                            material = reader.GetAttribute("material");
+                            form = reader.GetAttribute("form");
+                        }
+                    }
+                    else if (reader.NodeType == XmlNodeType.Text)
+                    {
+                        switch (element)
+                        {
+                            case "length":
+                                length = int.Parse(reader.Value);
+                                break;
+                            case "width":
+                                width = int.Parse(reader.Value);
+                                break;
+                            case "radius":
+                                radius = int.Parse(reader.Value);
+                                break;
+                        }
+                    }
+                    else if ((reader.NodeType == XmlNodeType.EndElement) && (reader.Name == "figure"))
+                    {
+                        if (index != figures.Length)
+                        {
+                            figures[index] = GetConcreteSheetFigure(material, form, length, width, radius);
+                            index++;
+                        }
+                        else
+                        {
+                            return figures;
+                        }
+                    }
+                }
+
+                return figures;
+            }
+        }
+
+        /// <summary>
+        /// Initialize array of figures.
+        /// </summary>
+        private IFigure[] InitFiguresArray()
+        {
+            IFigure[] figures = new IFigure[20];
+
+            for (int i = 0; i < figures.Length; i++)
+                figures[i] = null;
+
+            return figures;
+        }
+
+        /// <summary>
+        /// Returns figure from xml file.
+        /// </summary>
+        /// <param name="figure">Figure in xml.</param>
+        /// <returns></returns>
+        private IFigure GetFigure(XmlNode figure)
+        {
+            GetFigureSideValues(figure, out int length, out int width, out int radius);
+            GetFigureMaterialAndForm(figure, out string material, out string form);
+            return GetConcreteSheetFigure(material, form, length, width, radius);
+        }
+
+        /// <summary>
+        /// Returns sides of figure.
+        /// </summary>
+        /// <param name="figure">Figure.</param>
+        /// <param name="length">Length.</param>
+        /// <param name="width">Width.</param>
+        /// <param name="radius">Radius.</param>
+        private void GetFigureSideValues(XmlNode figure, out int length, out int width, out int radius)
+        {
+            length = 0;
+            width = 0;
+            radius = 0;
+
+            XmlNodeList sides = figure.ChildNodes;
+            foreach (XmlNode side in sides)
+            {
+                switch (side.Name)
+                {
+                    case "length":
+                        length = int.Parse(side.InnerText);
+                        break;
+                    case "width":
+                        width = int.Parse(side.InnerText);
+                        break;
+                    case "radius":
+                        radius = int.Parse(side.InnerText);
+                        break;
+                }
+            }
+
+            if (length == 0 && width == 0 && radius == 0)
+                throw new Exception("Incorrect values of figure's sides");
+        }
+
+        /// <summary>
+        /// Return material and form of figure
+        /// </summary>
+        /// <param name="figure">Figure.</param>
+        /// <param name="material">Material.</param>
+        /// <param name="form">Form.</param>
+        private void GetFigureMaterialAndForm(XmlNode figure, out string material, out string form)
+        {
+            material = "";
+            form = "";
+
+            foreach (XmlAttribute attribute in figure.Attributes)
+            {
+                switch (attribute.Name)
+                {
+                    case "material":
+                        material = attribute.InnerText;
+                        break;
+                    case "form":
+                        form = attribute.InnerText;
+                        break;
+                }
+            }
+
+            if (material == "" || form == "")
+                throw new Exception("Incorrect values of figure's material and sides");
+        }
+
+        /// <summary>
+        /// Return figure with specific material and form
+        /// </summary>
+        /// <param name="material">Figure's material.</param>
+        /// <param name="form">Figure's form.</param>
+        /// <param name="length">Figure's length.</param>
+        /// <param name="width">Figure's width.</param>
+        /// <param name="radius">Figure's radius.</param>
+        /// <returns></returns>
+        private IFigure GetConcreteSheetFigure(string material, string form, int length, int width, int radius)
+        {
+            IFigure figure = null;
+
+            if (material == "Paper")
+            {
+                if (form == "Rectangle")
+                    figure = new PaperRectangle(length, width);
+                if (form == "Circle")
+                    figure = new PaperCircle(radius);
+            }
+
+            if (material == "Film")
+            {
+                if (form == "Rectangle")
+                    figure = new FilmRectangle(length, width);
+                if (form == "Circle")
+                    figure = new FilmCircle(radius);
+            }
+            if (material == "Plastic")
+            {
+                if (form == "Rectangle")
+                    figure = new PlasticRectangle(length, width);
+                if (form == "Circle")
+                    figure = new PlasticCircle(radius);
+            }
+
+
+            if (figure == null)
+                throw new Exception("Incorrect data for figure");
+            else
+                return figure;
+        }
+    }
+}
---
+++ task03/FiguresBoxLib/Xml/XmlWriteOperation.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using FiguresLib.Interfaces;
+
+namespace FiguresBoxLib.Xml
+{
+    /// <summary>
+    /// Method enumerations for writing xml files
+    /// </summary>
+    public enum XmlWriteType
+    {
+        /// <summary>
+        /// StreamWriter.
+        /// </summary>
+        StreamWriter,
+        /// <summary>
+        /// XmlWriter.
+        /// </summary>
+        XmlWriter
+    }
+    /// <summary>
+    /// Enumeration of options for writing figures in xml file.
+    /// </summary>
+    public enum FigureWriteType
+    {
+        /// <summary>
+        /// All figures.
+        /// </summary>
+        All,
+        /// <summary>
+        /// Only paper.
+        /// </summary>
+        Paper,
+        /// <summary>
+        /// Only film.
+        /// </summary>
+        Film,
+        /// <summary>
+        /// Only plastic.
+        /// </summary>
+        Plastic
+    }
+    /// <summary>
+    /// Class describing writing xml file.
+    /// </summary>
+    internal class XmlWriteOperation
+    {
+
+        /// <summary>
+        /// Writing figures in xml file using StreamWriter.
+        /// </summary>
+        /// <param name="filePath">File path.</param>
+        /// <param name="figures">Array of figures.</param>
+        /// <param name="figureWriteType">Figure write type.</param>
+        /// <returns></returns>
+        public void WriteXmlFileWithStreamReader(string filePath, IFigure[] figures, FigureWriteType figureWriteType)
+        {
+            XmlDocument document = new XmlDocument();
+            document.AppendChild(document.CreateXmlDeclaration("1.0", "UTF-8", null));
+            XmlElement root = document.CreateElement("figures");
+
+            for (int i = 0; i < figures.Length; i++)
+            {
+                if (CheckFigure(figures[i], figureWriteType))
+                {
+                    DetermineFigureType(document, root, figures[i]);
+                }
+            }
+
+            document.AppendChild(root);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                document.Save(writer);
+            }
+        }
+
+        /// <summary>
+        /// Writing figures in xml file using  XmlWriter.
+        /// </summary>
+        /// <param name="filePath">File path.</param>
+        /// <param name="figures">Array of figures.</param>
+        /// <param name="figureWriteType">Figure write type.</param>
+        /// <returns></returns>
+        public void WriteXmlFileWithXmlReader(string filePath, IFigure[] figures, FigureWriteType figureWriteType)
+        {
+            using (XmlWriter writer = XmlWriter.Create(filePath))
+            {
+                string material = "";
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("figures");
+
+                foreach (IFigure figure in figures)
+                {
+                    if (CheckFigure(figure, figureWriteType))
+                    {
+                        writer.WriteStartElement("figure");
+
+                        switch (figure.GetType().GetInterfaces()[0].Name)
+                        {
+                            case "IRectangle":
+                                IRectangle rectangle = (IRectangle)figure;
+
+                                material = rectangle.GetType().BaseType.Name.Replace("Sheet", "");
+                                writer.WriteAttributeString("material", material);
+                                writer.WriteAttributeString("form", "Rectangle");
+
+                                writer.WriteStartElement("length");
+                                writer.WriteString(rectangle.Length.ToString());
+                                writer.WriteEndElement();
+
+                                writer.WriteStartElement("width");
+                                writer.WriteString(rectangle.Width.ToString());
+                                writer.WriteEndElement();
+
+                                break;
+
+                            case "ICircle":
+                                ICircle circle = (ICircle)figure;
+
+                                material = circle.GetType().BaseType.Name.Replace("Sheet", "");
+                                writer.WriteAttributeString("material", material);
+                                writer.WriteAttributeString("form", "Circle");
+
+                                writer.WriteStartElement("radius");
+                                writer.WriteString(circle.Radius.ToString());
+                                writer.WriteEndElement();
+
+                                break;
+                        }
+
+                        writer.WriteEndElement();
+                    }
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+        /// <summary>
+        /// Checking the figure for meeting the conditions.
+        /// </summary>
+        private bool CheckFigure(IFigure figure, FigureWriteType figureWriteType)
+        {
+            switch (figureWriteType)
+            {
+                case FigureWriteType.All:
+                    if (figure != null)
+                        return true;
+                    else
+                        return false;
+                case FigureWriteType.Film:
+                    if (figure != null && figure.GetType().BaseType.Name == "FilmSheet")
+                        return true;
+                    else
+                        return false;
+                case FigureWriteType.Paper:
+                    if (figure != null && figure.GetType().BaseType.Name == "PaperSheet")
+                        return true;
+                    else
+                        return false;
+                case FigureWriteType.Plastic:
+                    if (figure != null && figure.GetType().BaseType.Name == "PlasticSheet")
+                        return true;
+                    else
+                        return false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Defines figure type.
+        /// </summary>
+        /// <param name="document">XmlDocument.</param>
+        /// <param name="root">Root element.</param>
+        /// <param name="figure">Figure.</param>
+        private void DetermineFigureType(XmlDocument document, XmlElement root, IFigure figure)
+        {
+            string figureType = figure.GetType().GetInterfaces()[0].Name;
+
+            switch (figureType)
+            {
+                case "IRectangle":
+                    GetRectangleNode(document, root, (IRectangle)figure);
+                    break;
+                case "ICircle":
+                    GetCircleNode(document, root, (ICircle)figure);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Forms XmlElement of rectangle.
+        /// </summary>
+        /// <param name="document">XmlDocument.</param>
+        /// <param name="root">Root element.</param>
+        /// <param name="rectangle">Rectangle.</param>
+        private void GetRectangleNode(XmlDocument document, XmlElement root, IRectangle rectangle)
+        {
+            XmlElement rectangleElement = document.CreateElement("figure");
+
+            string material = rectangle.GetType().BaseType.Name.Replace("Sheet", "");
+            rectangleElement.SetAttribute("material", material);
+            rectangleElement.SetAttribute("form", "Rectangle");
+
+            XmlElement lengthElement = document.CreateElement("length");
+            lengthElement.InnerText = rectangle.Length.ToString();
+            rectangleElement.AppendChild(lengthElement);
+
+            XmlElement widthElement = document.CreateElement("width");
+            widthElement.InnerText = rectangle.Width.ToString();
+            rectangleElement.AppendChild(widthElement);
+
+            root.AppendChild(rectangleElement);
+        }
+
+        /// <summary>
+        /// Forms XmlElement of circle.
+        /// </summary>
+        /// <param name="document">XmlDocument.</param>
+        /// <param name="root">Root element.</param>
+        /// <param name="circle">Circle.</param>
+        private void GetCircleNode(XmlDocument document, XmlElement root, ICircle circle)
+        {
+            XmlElement circleElement = document.CreateElement("figure");
+
+            string material = circle.GetType().BaseType.Name.Replace("Sheet", "");
+            circleElement.SetAttribute("material", material);
+            circleElement.SetAttribute("form", "Circle");
+
+            XmlElement lengthElement = document.CreateElement("radius");
+            lengthElement.InnerText = circle.Radius.ToString();
+            circleElement.AppendChild(lengthElement);
+
+            root.AppendChild(circleElement);
+        }
+    }
+}
